49a05907b70e32fbacb2b4cc5629cf4e1685a649
xiongxi <turing.xiong@corp.to8to.com>
2020-10-09 14:18:06 +0800

    创建咖啡因跟踪小组件

---
+++ wired/wired/wiredApp.swift
@@ -0,0 +1,17 @@
+//
+//  wiredApp.swift
+//  wired
+//
+//  Created by 熊熙 on 2020/10/9.
+//
+
+import SwiftUI
+
+@main
+struct wiredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
