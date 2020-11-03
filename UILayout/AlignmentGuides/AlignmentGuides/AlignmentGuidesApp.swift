c74973e6172ec68861c80ff0047b8a224f4706af
xiongxi <turing.xiong@corp.to8to.com>
2020-11-03 10:11:46 +0800

    swiftUI布局指南

---
+++ UILayout/AlignmentGuides/AlignmentGuides/AlignmentGuidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignmentGuidesApp.swift
+//  AlignmentGuides
+//
+//  Created by 熊熙 on 2020/11/2.
+//
+
+import SwiftUI
+
+@main
+struct AlignmentGuidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
